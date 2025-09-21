936c6de3496e9de465700464de6fe8f64f271eb6
ddddd <fgx.900@gmail.com>
2025-09-21 13:49:48 +0300

    تحديث الموقع

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ export const Footer: React.FC<FooterProps> = ({ onLinkClick }) => {
             <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-8 text-sm">
                 <div className="space-y-4">
                      <div className="flex items-center gap-3">
-                        <img src="/public/logoo.png" alt="AI-PCBuilder Logo" className="site-logo"/>     
+                        <img src="/logoo.png" alt="AI-PCBuilder Logo" className="site-logo"/>     
                             </div>
                     <p className="max-w-xs">{t.hero.subtitle}</p>
                 </div>
